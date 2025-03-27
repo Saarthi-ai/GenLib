b0f7ca82c6a044420134f92a0bc8b9abcd2e6b33
Amrit Jha <amrit.jha@saarthi.ai>
2025-03-27 11:23:24 +0530

    comp added

--- demo/App.tsx
+++ demo/App.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import MyComponent from '../components/MyComponent';
 import Button from '../components/Button'; // Import Button component
 import AddButton from '../components/addButton/AddButton';
+import AddUserForm from '../components/addUser/AddUserForm';
 
 const App = () => {
   const handleClick = () => {
@@ -17,6 +18,12 @@ const App = () => {
         text={"Get it"}
         onCountChange={() => null} 
       />
+      {/* <AddUserForm
+        handleClickCancelAddUserModel={() =>
+          null
+        }
+        handleClickCreateUser={() => null}
+      /> */}
     </div>
   );
 };
