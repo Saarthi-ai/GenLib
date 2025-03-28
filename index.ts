9ed8195fa8dabc64bf5141f124e3fe16188bb9d4
Ekata Kumari <ekata.kumari@saarthi.ai>
2025-03-28 14:43:54 +0530

    add

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 
+export { default as ClockProgress } from "./components/clockProgress";
+// Add more component exports here as needed
+export { default as ImgComp } from "./components/imgComp";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-
 export { default as ClockProgress } from "./components/clockProgress";
+// Add more component exports here as needed
+export { default as ImgComp } from "./components/imgComp";
